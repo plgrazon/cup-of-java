74d0f062105695414e3e528ff32f0b1d4ede1950
plgrazon <plgrazon@gmail.com>
2022-10-02 11:49:13 -0400

    java - Add new exercises

---
+++ LeapYear/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LeapYear.isLeapYear(-1600)); // false
+        System.out.println(LeapYear.isLeapYear(1600)); // true
+        System.out.println(LeapYear.isLeapYear(2017)); // false
+        System.out.println(LeapYear.isLeapYear(2000)); // true
+    }
+}
